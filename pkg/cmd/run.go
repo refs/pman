f05b8833071aacc9a00a5becc3ced94dd9866ec6
Alex Unger <zyxancf@gmail.com>
2020-07-01 19:36:54 +0200

    add leftover aattr

--- pkg/cmd/run.go
+++ pkg/cmd/run.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"net"
 	"net/rpc"
+	"os"
 
 	"github.com/refs/pman/pkg/config"
 	"github.com/refs/pman/pkg/process"
@@ -35,7 +36,7 @@ func Run(cfg *config.Config) *cobra.Command {
 				log.Fatal("dialing:", err)
 			}
 
-			proc := process.NewProcEntry(args[0], []string{args[0]}...)
+			proc := process.NewProcEntry(args[0], os.Environ(), []string{args[0]}...)
 			var res int
 
 			if err := client.Call("Service.Start", proc, &res); err != nil {
